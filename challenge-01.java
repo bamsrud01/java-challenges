a91d1d5579b11d8814a421b287d6599fc9e85a36
Barrett Amsrud <barrettamsrud@gmail.com>
2017-02-22 23:21:07 -0600

    Completed challenge 1

---
+++ challenge-01.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    //  1. Create a variable to store the number of pounds
+    double numPounds = 225d;
+    //  2. Calculate the number of kilograms for the number and store in a variable
+      //  1 pound = 0.45359237
+    double convertedKilograms = numPounds * 0.45359237;
+    //  3. Print the result
+    System.out.println(numPounds + " pounds = " + convertedKilograms + " kilograms");
+  }
+}
